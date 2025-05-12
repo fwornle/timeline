e6b5a19e83422b4823e43e7722a07d65bdf379d6
fwornle <fwornle@yahoo.co.uk>
2025-05-12 15:28:00 +0200

    vite/react project initialized

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <Layout>
+      <div className="space-y-4">
+        <h2 className="text-xl font-semibold">Welcome to Timeline</h2>
+        <p className="text-gray-600">
+          This is the starting point of your timeline application.
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default App
